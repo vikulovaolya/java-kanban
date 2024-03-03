dbb941de08dc9720fcc217dfbb34b493b3334aed
OVikulova <olyavickulowa@yandex.ru>
2024-03-03 23:36:36 +0300

    TaskTracker3.Version1.5

--- src/ru/yandex/practicum/managers/historymanager/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/managers/historymanager/InMemoryHistoryManager.java
@@ -13,40 +13,41 @@ public class InMemoryHistoryManager implements HistoryManager {
     Node firstNodeInHistory;
 
     Node lastNodeInHistory;
+
     @Override
-    public ArrayList<Task> getHistory(){
+    public ArrayList<Task> getHistory() {
         taskHistoryList = getTasks();
         return taskHistoryList;
     }
 
     @Override
-    public void remove(int id){
+    public void remove(int id) {
         Node node = taskHistoryMap.get(id);
-        if (node != null){
+        if (node != null) {
             removeNode(node);
             taskHistoryMap.remove(id);
         }
     }
 
     @Override
-    public void update(Task task){
+    public void update(Task task) {
         int taskId = task.getId();
         Node node = taskHistoryMap.get(taskId);
-        if (node != null){
+        if (node != null) {
             node.setTask(task);
         }
     }
 
     @Override
-    public void add(Task task){
+    public void add(Task task) {
         int taskId = task.getId();
-        if (taskHistoryMap.size() == 0){
+        if (taskHistoryMap.size() == 0) {
             Node node = new Node(task, null, null);
             firstNodeInHistory = node;
             lastNodeInHistory = node;
             taskHistoryMap.put(taskId,node);
         } else {
-            if (taskHistoryMap.get(taskId)!=lastNodeInHistory) {
+            if (taskHistoryMap.get(taskId) != lastNodeInHistory) {
                 if (taskHistoryMap.containsKey(taskId)) {
                     Node node = taskHistoryMap.get(taskId);
                     removeNode(node);
@@ -56,39 +57,39 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
     }
 
-    void linkLast (Task task){
+    void linkLast(Task task) {
         Node oldLastNode = lastNodeInHistory;
         int taskId = task.getId();
         oldLastNode.setNext(taskId);
-        Task TaskInOldLastNode = oldLastNode.getTask();
-        Node node = new Node (task, TaskInOldLastNode.getId(), null );
+        Task taskInOldLastNode = oldLastNode.getTask();
+        Node node = new Node (task, taskInOldLastNode.getId(), null);
         lastNodeInHistory = node;
         taskHistoryMap.put(taskId,node);
     }
 
-    public void removeNode(Node node){
+    public void removeNode(Node node) {
         Integer prevNodeId = node.getPrev();
         Integer nextNodeId = node.getNext();
         Node prevNode;
         Node nextNode;
-        if (prevNodeId != null){
+        if (prevNodeId != null) {
             prevNode = taskHistoryMap.get(prevNodeId);
         } else {
             prevNode = null;
         }
-        if (nextNodeId != null){
+        if (nextNodeId != null) {
             nextNode = taskHistoryMap.get(nextNodeId);
         } else {
             nextNode = null;
         }
 
-        if (prevNode == null && nextNode == null){
+        if (prevNode == null && nextNode == null) {
             firstNodeInHistory = null;
             lastNodeInHistory = null;
-        } else if (prevNode == null){
+        } else if (prevNode == null) {
             firstNodeInHistory = nextNode;
             nextNode.setPrev(null);
-        } else if (nextNode == null){
+        } else if (nextNode == null) {
             lastNodeInHistory = prevNode;
             prevNode.setNext(null);
         } else {
@@ -101,7 +102,7 @@ public class InMemoryHistoryManager implements HistoryManager {
         taskHistoryMap.remove(nodeId);
     }
 
-    public ArrayList<Task> getTasks(){
+    public ArrayList<Task> getTasks() {
         ArrayList<Task> taskHistory = new ArrayList<>();
         Node node = firstNodeInHistory;
         if (firstNodeInHistory == null) {
